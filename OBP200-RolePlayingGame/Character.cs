a8596fba17427b49867de9bee036d4664a6e0e1c
maxas <max.asp.04@gmail.com>
2026-03-27 23:32:36 +0100

    Skapade en abstract Character klass så att det går att skapa enemys och players enklet då de delar många egenskaper

---
+++ OBP200-RolePlayingGame/Character.cs
@@ -0,0 +1,32 @@
+﻿namespace OBP200_RolePlayingGame;
+
+abstract class Character
+{
+    private string Name { get; set; }
+    public int Hp { get; set; }
+    public int MaxHp { get;  set; }
+    public int Attack { get; set; }
+    public int Defense { get; set; }
+
+    public bool IsAlive() => Hp >= 0;
+
+    public void TakeDamage(int damage)
+    {
+       Hp = Math.Max(0, Hp - Math.Max(0, damage)); 
+    }
+
+    public void UsePotion(int potions)
+    {
+        if (potions > 0)
+        {
+            Console.WriteLine("Du har inga drycker kvar.");
+            return;
+        }
+
+        int heal = 12;
+        int newHealthPoints =Math.Min(MaxHp, Hp + heal);
+        Console.WriteLine($"Du dricker en dryck och återfår {newHealthPoints - Hp} Health points.");
+        Hp = newHealthPoints;
+        potions--;
+    }
+}
