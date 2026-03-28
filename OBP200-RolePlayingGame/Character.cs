7f9c5df3d43d4ed5d4d99211594e2a73c3a19e76
maxas <max.asp.04@gmail.com>
2026-03-28 16:53:30 +0100

    Changed the set from public to protected to do some encapsulation

--- OBP200-RolePlayingGame/Character.cs
+++ OBP200-RolePlayingGame/Character.cs
@@ -1,32 +1,26 @@
 ﻿namespace OBP200_RolePlayingGame;
 
-abstract class Character
+public abstract class Character
 {
-    private string Name { get; set; }
-    public int Hp { get; set; }
-    public int MaxHp { get;  set; }
-    public int Attack { get; set; }
-    public int Defense { get; set; }
+    public string Name { get; protected set; }
+    public int Hp { get; protected set; }
+    public int MaxHp { get;  protected set; }
+    public int Attack { get; protected set; }
+    public int Defense { get; protected set; }
 
-    public bool IsAlive() => Hp >= 0;
+    public bool IsAlive() => Hp > 0;
+
+    public Character(string name, int hp, int maxHp, int attack, int defense)
+    {
+        Name = name;
+        Hp = hp;
+        MaxHp = maxHp;
+        Attack = attack;
+        Defense = defense;
+    }
 
     public void TakeDamage(int damage)
     {
        Hp = Math.Max(0, Hp - Math.Max(0, damage)); 
-    }
-
-    public void UsePotion(int potions)
-    {
-        if (potions > 0)
-        {
-            Console.WriteLine("Du har inga drycker kvar.");
-            return;
-        }
-
-        int heal = 12;
-        int newHealthPoints =Math.Min(MaxHp, Hp + heal);
-        Console.WriteLine($"Du dricker en dryck och återfår {newHealthPoints - Hp} Health points.");
-        Hp = newHealthPoints;
-        potions--;
     }
 }
