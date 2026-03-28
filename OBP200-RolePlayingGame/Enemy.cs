eb5439fbec905d0e2c3e1f83b12bf226406bb918
maxas <max.asp.04@gmail.com>
2026-03-28 16:50:23 +0100

    I created a Enemy chall that has inheritence from the class Character. In the enemy class I implemented all the nessery methods that was needed and transfered them from the Main class to the enemy class.

---
+++ OBP200-RolePlayingGame/Enemy.cs
@@ -0,0 +1,59 @@
+﻿namespace OBP200_RolePlayingGame;
+
+public class Enemy : Character
+{
+    public string Type { get; private set; }
+    public int XPReward { get; private set; }
+    public int GoldReward { get; private set; }
+    public bool IsBoss { get; private set; }
+
+    public Enemy(string type, string name, int hp, int maxHp, int attack, int defense, int xpReward, int goldReward,
+        bool isBoss) : base(name, hp, maxHp, attack, defense)
+    {
+        Type = type;
+        XPReward = xpReward;
+        GoldReward = goldReward;
+        IsBoss = isBoss;
+    }
+
+    public void Appered()
+    {
+        Console.WriteLine($"En {Name} dyker upp! (HP {Hp}, ATK {Attack}, DEF {Defense})");
+    }
+
+    public void TakeDamage(int damage)
+    {
+        Hp -= damage;
+    }
+
+    public void ResetHp()
+    {
+        Hp = MaxHp;
+    }
+    
+    public int CalculateDamage(Random random, int playerDefense)
+    {
+        int roll = random.Next(0, 3);
+        int dmg = Math.Max(1, Attack - (playerDefense / 2)) + roll;
+
+        return dmg;
+    }
+
+    public void RandomizeStats(Random random)
+    {
+        Hp = MaxHp + random.Next(-1, 3);
+        Attack += random.Next(0, 2);
+        Defense += random.Next(0, 2);
+        XPReward += random.Next(0, 3);
+        GoldReward += random.Next(0, 3);
+    }
+
+    public void MaybeGivePlayerLoot(Random rng, IInventoryAdd inventoryAdd)
+    {
+        if (rng.NextDouble() < 0.35)
+        {
+            string item = IsBoss ? "Dragon Scale" : "Minor Gem";
+            inventoryAdd.AddToInventory(item);
+        }
+    }
+}
